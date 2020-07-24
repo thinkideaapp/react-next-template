38d0d1ca3aa1daf31f6b09481cdf612e138a576d
Kevio Castro <keviocastro@gmail.com>
2020-07-24 14:55:17 -0300

    Nextjs typescript eslint pretiffier

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Hi</title>
+      </Head>
+      
+      <main>
+        <h1>Hi</h1>
+      </main>
+    </div>
+  )
+}
